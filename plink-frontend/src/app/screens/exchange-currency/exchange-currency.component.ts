1fb272e09e0a3b7ebcc0bf7716da0f191ca29a59
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-12-12 12:13:04 -0500

    convert logical currency

--- plink-frontend/src/app/screens/exchange-currency/exchange-currency.component.ts
+++ plink-frontend/src/app/screens/exchange-currency/exchange-currency.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { CurrencyService } from 'src/app/services/currency.service';
 import { Currency } from 'src/app/models/currency.model';
+import { Convert } from 'src/app/models/convert.model';
 
 @Component({
   selector: 'app-exchange-currency',
@@ -8,15 +10,35 @@ import { Currency } from 'src/app/models/currency.model';
 })
 export class ExchangeCurrencyComponent implements OnInit, OnChanges {
 
-  @Input() prices: Currency;
+  @Input() prices: Currency[];
+  convert: Convert = { value: 0, to: 'USD', from: 'BTC', quantity: 0 };
 
-  constructor() { }
+  constructor(private currencyService: CurrencyService) {
+  }
 
   ngOnInit() {
+    this.prices.push({
+      id_currency: 'BTC',
+      name: 'Bitcoin',
+      price: 1,
+      crypto: 1
+    });
   }
 
   ngOnChanges() {
-    console.log('exchange component', this.prices);
   }
 
+  convertCurrency() {
+    this.currencyService.getConvert(this.convert).subscribe(({ to_quantity }: any) => {
+      this.convert.quantity = to_quantity;
+    })
+  }
+
+  changeSelect() {
+    const { to, from } = this.convert;
+    const tempTo = to;
+    this.convert.to = from
+    this.convert.from = tempTo;
+    this.convertCurrency();
+  }
 }
